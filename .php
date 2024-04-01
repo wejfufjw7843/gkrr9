0b6b6152051a1a61336c6632758c4c3707688fec
wejfufjw7843 <163233954+wejfufjw7843@users.noreply.github.com>
2024-04-01 10:11:24 +0500

    Add files via upload

---
+++ .php
@@ -0,0 +1,11 @@
+<?php
+def sum_numbers(num1, num2):
+    """Возвращает сумму двух чисел."""
+    return num1 + num2
+
+
+# Вызываем функцию с разными парами чисел
+print(sum_numbers(1, 2))  # Выведет 3
+print(sum_numbers(3.5, 4.7))  # Выведет 8.2
+print(sum_numbers(-10, 20))  # Выведет 10
+?>
